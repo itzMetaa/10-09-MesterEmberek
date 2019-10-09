336ebd57207f9d9c27ec912166ed50153c2fd35f
Diak <Diak@10.4.117.8>
2019-10-09 15:48:29 +0200

    publish not finished

--- src/mesteremberek/MesterEmberek.java
+++ src/mesteremberek/MesterEmberek.java
@@ -1,12 +1,10 @@
-
 package mesteremberek;
 
 
 public class MesterEmberek {
 
-
     public static void main(String[] args) {
-
+        
     }
     
 }
---
+++ src/mesteremberek/Mesterember.java
@@ -0,0 +1,9 @@
+package mesteremberek;
+
+public abstract class Mesterember {
+    private String nev;
+    private int napiDij;
+    private boolean[] foglaltNapok;
+    
+    
+}
